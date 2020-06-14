f679b3a3fe31dff8d5cafcd3b91c08a080f2559f
varunkondreddy <45779365+varunkondreddy@users.noreply.github.com>
2020-06-14 17:03:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from flask import Flask, request, jsonify
 import datetime
 import pickle
 import os.path
@@ -8,8 +9,12 @@ from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-app = Flask(__name__)
 
+app = Flask(__name__)
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -22,7 +27,7 @@ app = Flask(__name__)
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file( 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
