a11419e3b799906b6662ffde3d2d0cf593bba291
varunkondreddy <45779365+varunkondreddy@users.noreply.github.com>
2020-06-16 10:31:11 +0530

    changes

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from __future__ import print_function
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import datetime
 import pickle
+import json
 import os.path 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 app = Flask(__name__)
-@app.route('/calendar', methods=["GET", "POST"])
+
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
+
+@app.route('/', methods=["GET", "POST"])
 def main(): 
 
     creds = None
+    content = json.loads(request.data)
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
@@ -26,29 +32,34 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=5000)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
+    event={'summary':content['summary'],
+    'location':content['location'],
+    'start':{
+        'dateTime': content['stime'],
+        'timeZone': 'Asia/Kolkata',
+    },
+      'end': {
+          'dateTime': content['etime'],
+          'timeZone': 'Asia/Kolkata',
+  },
+    'attendees': [
+    {'email': content['firstemail']},
+    {'email': content['secondemail']},
+    {'email': content['thirdemail']}
+  ],}
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print ('Event created: %s' % (event.get('htmlLink')))
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
 
+    return 'a string'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
